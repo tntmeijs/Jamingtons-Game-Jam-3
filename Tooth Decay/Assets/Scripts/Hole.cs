053255c7c174dd88271616e3589d490216905238
tntmeijs <tntmeijs@gmail.com>
2017-09-30 17:12:49 +0200

    Initial commit hole script

---
+++ Tooth Decay/Assets/Scripts/Hole.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+}
