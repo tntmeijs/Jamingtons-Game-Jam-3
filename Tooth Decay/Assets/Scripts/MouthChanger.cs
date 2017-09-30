9fb9c48b8bfe350b4ee704b48f3fef0ff28f2e50
tntmeijs <tntmeijs@gmail.com>
2017-09-30 16:23:29 +0200

    Mouth changer script
    First commit for the script. This script will allow us to change the mouth sprite.

---
+++ Tooth Decay/Assets/Scripts/MouthChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouthChanger : MonoBehaviour
+{
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
