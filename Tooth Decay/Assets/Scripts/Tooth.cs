05f8d941c119b3b801c99f4d632aee481e4ffcb8
tntmeijs <tntmeijs@gmail.com>
2017-09-30 16:42:38 +0200

    Tooth script
    This script will be used to store all data for a tooth.

---
+++ Tooth Decay/Assets/Scripts/Tooth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tooth : MonoBehaviour
+{
+
+    private GameObject damageSpawnerReference;
+    private int holesLeftToPath;
+    private int holesPathed;
+
+    // Use this for initialization
+    private void Start()
+    {
+        // Spawn the holes
+        GameObject.Find("DamageSpawner").GetComponent<DamageSpawner>().spawnDamage();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+}
