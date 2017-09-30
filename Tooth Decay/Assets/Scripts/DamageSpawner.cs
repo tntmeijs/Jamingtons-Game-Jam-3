9f1045f62455885763e580f87010dfbb714fb6e9
tntmeijs <tntmeijs@gmail.com>
2017-09-30 14:15:06 +0200

    Loading the damaged sprites
    The sprites have been moved to a resource folder to allow me to access the sub-sprites programmatically.

---
+++ Tooth Decay/Assets/Scripts/DamageSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSpawner : MonoBehaviour
+{
+
+    public Texture2D spriteSheet;
+
+    private Sprite[] sprites;
+
+    // Use this for initialization
+    private void Start()
+    {
+        sprites = Resources.LoadAll<Sprite>(spriteSheet.name);
+        Debug.Log(sprites.Length);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
