73b11d64c32024be6b1c5c61bc0e9df863abbb92
tntmeijs <tntmeijs@gmail.com>
2017-10-01 12:31:44 +0200

    Added spring interpolation
    Default parameter values have been added as well.

--- Tooth Decay/Assets/Scripts/Utils.cs
+++ Tooth Decay/Assets/Scripts/Utils.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Utils : MonoBehaviour {
 
-    static public float interpolateAnticipate(float fractionNormalized)
+    static public float interpolateAnticipate(float fractionNormalized, float tension = 0.4f)
     {
-        float tension = 4.0f;
         return fractionNormalized * fractionNormalized * ((tension + 2.0f) * fractionNormalized - tension);
+    }
+
+    static public float interpolateSpring(float fractionNormalized, float factor = 0.6f)
+    {
+        return Mathf.Pow(2.0f, -10.0f * fractionNormalized) * Mathf.Sin((fractionNormalized - factor / 4.0f) * (2.0f * Mathf.PI) / factor) + 1.0f;
     }
 
 }
