ad62d9f76103b14a14ab1ac0c808945c499443c8
tntmeijs <tntmeijs@gmail.com>
2017-10-01 13:39:15 +0200

    Proper transitions

--- Tooth Decay/Assets/Scripts/Tooth.cs
+++ Tooth Decay/Assets/Scripts/Tooth.cs
@@ -6,10 +6,23 @@ public class Tooth : MonoBehaviour
 {
 
     public bool isFixed = false;
+    public float interpolationTimeSeconds = 2.5f;
+
+    private bool shouldInterpolate;
+    private float targetScale;
+    private float interpolationAccumulator;
 
     // Use this for initialization
     private void Start()
     {
+        // The scale is uniform, so x == y
+        targetScale = transform.localScale.x;
+
+        // Reset the scale (this will be increased by using the interpolation function below
+        transform.localScale = new Vector2(0.0f, 0.0f);
+
+        shouldInterpolate = true;
+        interpolationAccumulator = 0.0f;
     }
 
     private void LateUpdate()
@@ -22,6 +35,19 @@ public class Tooth : MonoBehaviour
 
     private void Update()
     {
+        if (shouldInterpolate)
+        {
+            interpolationAccumulator += Time.deltaTime / interpolationTimeSeconds;
+
+            if (interpolationAccumulator >= 1.0f)
+            {
+                interpolationAccumulator = 1.0f;
+                shouldInterpolate = false;
+            }
+
+            float newScale = Utils.interpolateSpring(interpolationAccumulator) * targetScale;
+            transform.localScale = new Vector2(newScale, newScale);
+        }
     }
 
 }
