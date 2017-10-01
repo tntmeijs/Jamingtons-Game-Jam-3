7521aff74535606507ba74716a336b5e167a6990
tntmeijs <tntmeijs@gmail.com>
2017-10-01 22:57:20 +0200

    Final game

--- Tooth Decay/Assets/Scripts/EndGame.cs
+++ Tooth Decay/Assets/Scripts/EndGame.cs
@@ -8,12 +8,33 @@ public class EndGame : MonoBehaviour
 {
 
     public Text finalScoreLabel;
+    public Text highScore;
+
+    private static int previousHighScore = 0;
 
     // Use this for initialization
     private void Start()
     {
         UnityEngine.Cursor.visible = true;
         finalScoreLabel.text = "You managed to score " + GameManager.score.ToString() + " points!";
+
+        if (GameManager.score > previousHighScore)
+        {
+            if (previousHighScore == 0)
+            {
+                highScore.text = "You've set the first high-score, can you beat it?";
+            }
+            else
+            {
+                highScore.text = "You've beaten the high-score (" + previousHighScore + ") by" + ((int)(((float)previousHighScore / GameManager.score) * 100)).ToString() + "%!";
+            }
+
+            previousHighScore = GameManager.score;
+        }
+        else
+        {
+            highScore.text = "High score: " + GameManager.score + ".";
+        }
     }
 
     public void restart()
